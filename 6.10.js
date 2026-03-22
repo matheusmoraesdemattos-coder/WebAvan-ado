0e8c96a3b663eb60f240c0398a3a9825282b3401
matheusmoraesdemattos-coder <matheusmoraesdemattos@gmail.com>
2026-03-22 19:03:06 -0300

    Update 6.10.js

--- 6.10.js
+++ 6.10.js
@@ -6,10 +6,50 @@ const pedidos = [
   { id: 5, cliente: "Bruno", total: 150, status: "aprovado" }
 ];
 
+const pedidosaprovados= pedidos.filter(pedido => pedido.status === "aprovado") .map(pedido=> pedido.cliente)
+
+const valordevendas= pedidos.filter(pedido=> pedido.status === "aprovado") .reduce ((a, b) => a+b.total, 0) 
 
 
-const pedidoaprovados=pedidos.filter(pedido => pedido.status === "aprovado")
-.map(pedido => pedido.cliente);
-console.log("APROVADOS=\n", pedidoaprovados)
+const media= valordevendas / pedidosaprovados.length
+
+
+
+console.log(pedidosaprovados)
+console.log(valordevendas)
+console.log(media)
+
+
+class pedido  {
+    constructor (id, cliente, total, status){
+        this.id=id
+        this.cliente=cliente
+        this.total=total
+        this.status=status
+    }
+isAprovado() {
+   return this.status === "aprovado"
+}
+}
+const pedido2=[ new pedido (1,"matheus",100,"aprovado"),
+]
+
+
+
+console.log(pedido2,"\n", pedido2[0].isAprovado())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
